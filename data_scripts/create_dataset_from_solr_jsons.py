90ba050faba54048ed92e2ca1e372a4971d75bfa
azad96 <azad.baykara96@gmail.com>
2023-01-06 18:19:01 +0100

    script for creating dataset from solr jsons

---
+++ data_scripts/create_dataset_from_solr_jsons.py
@@ -0,0 +1,31 @@
+import json
+import os
+import os.path as osp
+import shutil
+from pathlib import Path
+from tqdm import tqdm
+
+
+labels = ['bench', 'cabinet', 'faucet', 'stove', 'bookshelf', 'computer', 'desk', 'chair', 'monitor', 'sofa', 'lamp', 'nightstand', 'bed', 'dishwasher', 'fridge', 'microwave', 'toilet']
+jsons_dir = '/home/baykara/adl4cv/pointnet_pytorch/data/solr_jsons'
+out_data_dir = '/home/baykara/adl4cv/pointnet_pytorch/data/adl_shapenet'
+org_shapenet_dir = '/home/baykara/adl4cv/pointnet_pytorch/data/shapenetv2'
+
+for label in tqdm(labels):
+    file_path = osp.join(jsons_dir, f'{label}.json')
+    with open(file_path, 'r') as f:
+        json_file = json.load(f)
+        instances = json_file['response']['docs']
+
+        # create a folder for the current class
+        class_path = osp.join(out_data_dir, label)
+        Path(class_path).mkdir(parents=True, exist_ok=True)
+
+        for i,instance in enumerate(instances):
+            synsetId = instance['synsetId']
+            fullId = instance['fullId'][0].split('.')[-1]
+            instance_path = osp.join(org_shapenet_dir, synsetId, fullId, 'models/model_normalized.obj')
+            out_file_path = osp.join(class_path, f'{label}{i}.obj')
+            shutil.copyfile(instance_path, out_file_path)
+
+            
